f315b8a75ff0b39d08a9b865642d0624d4674a79
jessclark1234 <jessicaclark7890@gmail.com>
2023-11-12 17:21:34 -0800

    added more to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,26 @@
-function getWeatehrApi() {
-
+function getWeatherApi() {
     var weatherApiKey = '877db42c219339bc7225168a019787ed'
-    var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}';
-
+    var cityName = document.getElementById('searchBar').value
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + weatherApiKey
     fetch(requestUrl)
-    .then(function (response) {
-        return response.json;
-    })
-    .then(function (data) {
-        console.log(data)
-        for (var i = 0; i < data.length; i++) {
-            var get
-        }
-    })
-}
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            for (var i = 0; i < data.list.length; i=i+8)  {
+                var day = data.list[i]
+                // document.getElementById('day' + (i/8+1)).textContent = day.dt_txt
+                document.getElementById('temp' + (i/8+1)).textContent = 'Temperature: ' + day.main.temp + '*F'
+                // localStorage.setItem("Temp",Temp)
+                document.getElementById('wind' + (i/8+1)).textContent = 'Wind Speed: ' + day.wind.speed + 'mph'
+                document.getElementById('humidity' + (i/8+1)).textContent = 'Humidity: ' +day.main.humidity + '%'
+            }
+        })
+};
 
-function dayjs () {
-var today = dayjs();
-$(currentDay).text(today.format('dddd, M DD, YY'));
-}
+   document.getElementById("searchBtn") .addEventListener('click', function () {
+    getWeatherApi();
+   })
+
+
